e65b7f3362c933690b7630a914c1831d2754007a
roger <rjpd@netzero.com>
2017-10-16 23:19:56 -0400

    fmt and gae_updt

--- helloWin.go
+++ helloWin.go
@@ -1,26 +1,19 @@
 package hello
 
-
-
 import (
 	"fmt"
-    
-	"net/http"
 
+	"net/http"
 )
 
-
-
 func init() {
-    
+
 	http.HandleFunc("/", handler)
 
 }
 
-
-
 func handler(w http.ResponseWriter, r *http.Request) {
-    	
+
 	fmt.Fprint(w, "Hello, world Win!")
 
 }
