eef4f506c4ecbd80aac0ad772af420845af8d6e8
roger <rjpd@netzero.com>
2017-11-21 19:50:01 -0500

    Hello gae init

--- helloWin.go
+++ helloWin.go
@@ -1,9 +1,13 @@
 package hello
 
 import (
-	"fmt"
-
 	"net/http"
+	//C:\Users\roger\AppData\Local\Google\Cloud SDK\google-cloud-sdk\platform\google_appengine\google\appengine\datastore
+	_ "golang.org/x/net/context" //as of 1.7 it is std lib, but gae may not be using latest(1.7)
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	//"appengine/datastore"
 )
 
 func init() {
@@ -12,8 +16,18 @@ func init() {
 
 }
 
+type (
+	Entity struct {
+		Value string
+	}
+)
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	//watch: file loc
+	//fmt.Fprint(w, "Hello, world Windows!!!")
 
-	fmt.Fprint(w, "Hello, world Win!")
-
+	ctx := appengine.NewContext(r)
+	k := datastore.NewKey(ctx, "Entity", "", 0, nil)
+	e := new(Entity)
+	datastore.Put(ctx, k, e)
 }
