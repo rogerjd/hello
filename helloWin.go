907c80e2ad83d9a23f2ce8335833b7a039a35e6a
roger <rjpd@netzero.com>
2017-11-21 19:53:36 -0500

    write to struct field

--- helloWin.go
+++ helloWin.go
@@ -29,5 +29,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	k := datastore.NewKey(ctx, "Entity", "", 0, nil)
 	e := new(Entity)
+	e.Value = "gae test"
 	datastore.Put(ctx, k, e)
 }
